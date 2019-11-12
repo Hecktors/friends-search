0249fd63a11549e83f8185f9f1aa17f061ae9c65
t <hectors.dev@gmail.com>
2019-11-12 13:22:37 -0400

    Updates

---
+++ precache-manifest.84c4eb261983eedf67ccad0f4b76011c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07472cdd8ac25092e75d5ac8fc13da6b",
+    "url": "/friend-search/index.html"
+  },
+  {
+    "revision": "64a84a663d485b72c1e0",
+    "url": "/friend-search/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "36faef9855ae86621a0a",
+    "url": "/friend-search/static/css/main.09976274.chunk.css"
+  },
+  {
+    "revision": "64a84a663d485b72c1e0",
+    "url": "/friend-search/static/js/2.6a535fe4.chunk.js"
+  },
+  {
+    "revision": "36faef9855ae86621a0a",
+    "url": "/friend-search/static/js/main.f4df392b.chunk.js"
+  },
+  {
+    "revision": "4167388c53a778c152ed",
+    "url": "/friend-search/static/js/runtime-main.11a0258f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/friend-search/static/media/SEGA.12a3db8b.woff"
+  }
+]);
