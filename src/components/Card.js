da733ab943a09cd39ecbdbbab872355e59665293
t <hectors.dev@gmail.com>
2019-12-11 11:36:16 -0400

    optimised card padding

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,9 +11,9 @@ const Card = ({ id, first, last, email, pic, country, city }) => {
         // </div>
         <article className="card ma1 dib br3 pa1 pa3-ns ba b--black-10">
             <div className="tc">
-                <img src={pic} className="br-100 w4 dib ba b--black-05 pa1" alt="friend" />
-                <h3 className="f5 mb2">{first}<br/>{last}</h3>
-                <p className="fz-sm fw4 dark-gray mt0">{`${city}, ${country}`}</p>
+                <img src={pic} className="br-100 w3 w4-ns dib ba b--black-05 pa1" alt="friend" />
+                <h3 className="f5 mt1 mt3-ns mb1 mb2-ns">{first}<br/>{last}</h3>
+                <p className="fz-sm fw4 dark-gray mt0 mb2 mb3-ns">{`${city}, ${country}`}</p>
             </div>
         </article>
     )
