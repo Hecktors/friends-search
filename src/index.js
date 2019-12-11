16ed03f1c3618ace26aa9d4290dcb04ca11fda94
t <hectors.dev@gmail.com>
2019-12-11 09:57:50 -0400

    registered service worker

--- src/index.js
+++ src/index.js
@@ -18,4 +18,4 @@ const store = createStore(rootReducer, applyMiddleware(thunk))
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
-serviceWorker.unregister();
+serviceWorker.register();
